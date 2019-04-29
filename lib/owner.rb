beb015430715fc852f4dd0cd0f55d1f666d96021
Jasterix C <loune.calixte@gmail.com>
2019-04-29 15:02:10 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
 class Owner
   # code goes here
+  COUNT = []
+  ALL = []
+  attr_reader :name
+  attr_accessor :mood
+  
+  def initialize(species)
+    @species = species
+    ALL << self
+    COUNT += 1
+  end
+    
+  def species
+    
+  end
+  
+  def self.all
+    ALL
+  end
+  
 end
